401345eaf8f034a0b10c258cb2132acfef3d3cb7
Al-Farsi <120342697+Somespi@users.noreply.github.com>
2024-09-29 23:03:32 +0400

    finialize MVP of landing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,20 @@
 <script lang="ts">
-  import DropDownSelect from './DropDownSelect.svelte';
+	import { goto } from '$app/navigation';
+    import DropDownSelect from './DropDownSelect.svelte';
 
 	$: selectedGrade = 'Unselected';
 	let grades: string[] = ['Elementary', 'Middle-School', 'High-School', 'College'];
+
+    function transitionToNextPage() {
+        window.sessionStorage.setItem('grade', selectedGrade.toLowerCase());
+        goto('/explore');
+    }
 </script>
 
 <div class="flex flex-col items-center justify-center content-center h-screen w-screen">
 	<div class="flex flex-col gap-y-16">
 		<DropDownSelect bind:selected={selectedGrade} options={grades}/>
-        <button class="btn btn-primary" disabled={selectedGrade == "Unselected"}>
+        <button class="btn btn-primary" disabled={selectedGrade == "Unselected"} on:click={transitionToNextPage}>
             Explore the Sky
         </button>
 	</div>
