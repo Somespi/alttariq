bc29e4b76b8cbdf1b53ea095396e77f930ef8a41
Al-Farsi <120342697+Somespi@users.noreply.github.com>
2024-09-29 22:59:30 +0400

    Implement MVP for landing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,16 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import DropDownSelect from './DropDownSelect.svelte';
+
+	$: selectedGrade = 'Unselected';
+	let grades: string[] = ['Elementary', 'Middle-School', 'High-School', 'College'];
+</script>
+
+<div class="flex flex-col items-center justify-center content-center h-screen w-screen">
+	<div class="flex flex-col gap-y-16">
+		<DropDownSelect bind:selected={selectedGrade} options={grades}/>
+        <button class="btn btn-primary" disabled={selectedGrade == "Unselected"}>
+            Explore the Sky
+        </button>
+	</div>
+
+</div>
